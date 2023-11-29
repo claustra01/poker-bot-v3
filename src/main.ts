5786e7c0af01167d4bdba474efdb0f4bdf6e98ae
claustra01 <shinya430.g@gmail.com>
2023-11-29 16:22:22 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { GatewayIntentBits, Client, Partials, Message } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Message, Partials.Channel],
+})
+
+client.once('ready', () => {
+    console.log('Ready!')
+    if(client.user){
+        console.log(client.user.tag)
+    }
+})
+
+client.on('messageCreate', async (message: Message) => {
+    if (message.author.bot) return
+    if (message.content === '!hello') {
+        message.channel.send("Hello!");
+    }
+})
+
+client.login(process.env.BOT_TOKEN)

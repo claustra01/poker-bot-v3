51908f84623c7e7bcb6e64e36ca93d497cb92882
claustra01 <shinya430.g@gmail.com>
2023-12-14 13:42:59 +0900

    [add] permitted role config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,12 +2,14 @@ import settings from '../../application-settings.json';
 
 export class Config {
   commandPrefix: string;
+  permittedRole: string;
   initialRate: number;
   weightedCoefficient: number;
   calculatingGameCount: number;
 
   constructor() {
     this.commandPrefix = './';
+    this.permittedRole = 'admin';
     this.initialRate = 1500;
     this.weightedCoefficient = 4;
     this.calculatingGameCount = 3;
@@ -15,6 +17,7 @@ export class Config {
 
   load(): void {
     this.commandPrefix = settings.commandPrefix;
+    this.permittedRole = settings.permittedRole;
     this.initialRate = settings.initialRate;
     this.weightedCoefficient = settings.weightedCoefficient;
     this.calculatingGameCount = settings.calculatingGameCount;

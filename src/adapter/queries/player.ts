a160e6833b124f3648c8985561f2f21e831ffa18
claustra01 <shinya430.g@gmail.com>
2023-12-18 01:33:32 +0900

    [fix] discord_id is nullable

--- src/adapter/queries/player.ts
+++ src/adapter/queries/player.ts
@@ -11,14 +11,15 @@ export class PlayerController implements IController<Player, NewPlayer> {
   }
 
   async create(newData: NewPlayer): Promise<Player> {
-    const result = await this.pool
-      .query(
-        `INSERT INTO players (player_name, discord_id) VALUES ($1, $2) RETURNING *`,
-        [newData.playerName, newData.discordId || '']
-      )
-      .catch((error) => {
-        throw error;
-      });
+    const query = newData.discordId
+      ? `INSERT INTO players (player_name, discord_id) VALUES ($1, $2) RETURNING *`
+      : `INSERT INTO players (player_name, discord_id) VALUES ($1, NULL) RETURNING *`;
+    const values = newData.discordId
+      ? [newData.playerName, newData.discordId]
+      : [newData.playerName];
+    const result = await this.pool.query(query, values).catch((error) => {
+      throw error;
+    });
     return parsePlayer(result[0]);
   }
 
@@ -78,12 +79,13 @@ export class PlayerController implements IController<Player, NewPlayer> {
   }
 
   async update(replaceData: Player): Promise<Player> {
-    const result = await this.pool
-      .query(
-        `UPDATE players SET discord_id = $2, current_rate = $3, max_rate = $4, game_count = $5, first_win_count = $6, second_win_count = $7, third_win_count = $8 WHERE player_name = $1 RETURNING *`,
-        [
+    const query = replaceData.discordId
+      ? `UPDATE players SET discord_id = $2, current_rate = $3, max_rate = $4, game_count = $5, first_win_count = $6, second_win_count = $7, third_win_count = $8 WHERE player_name = $1 RETURNING *`
+      : `UPDATE players SET discord_id = NULL, current_rate = $2, max_rate = $3, game_count = $4, first_win_count = $5, second_win_count = $6, third_win_count = $7 WHERE player_name = $1 RETURNING *`;
+    const values = replaceData.discordId
+      ? [
           replaceData.playerName,
-          replaceData.discordId || '',
+          replaceData.discordId,
           replaceData.currentRate.toString(),
           replaceData.maxRate.toString(),
           replaceData.gameCount.toString(),
@@ -91,10 +93,18 @@ export class PlayerController implements IController<Player, NewPlayer> {
           replaceData.secondWinCount.toString(),
           replaceData.thirdWinCount.toString(),
         ]
-      )
-      .catch((error) => {
-        throw error;
-      });
+      : [
+          replaceData.playerName,
+          replaceData.currentRate.toString(),
+          replaceData.maxRate.toString(),
+          replaceData.gameCount.toString(),
+          replaceData.firstWinCount.toString(),
+          replaceData.secondWinCount.toString(),
+          replaceData.thirdWinCount.toString(),
+        ];
+    const result = await this.pool.query(query, values).catch((error) => {
+      throw error;
+    });
     return parsePlayer(result[0]);
   }
 

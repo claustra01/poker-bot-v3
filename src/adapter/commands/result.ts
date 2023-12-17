d04a50775178809602223e3ef7ee465a5650f770
claustra01 <shinya430.g@gmail.com>
2023-12-17 20:59:51 +0900

    [wip] add players validation

--- src/adapter/commands/result.ts
+++ src/adapter/commands/result.ts
@@ -1,10 +1,12 @@
 import { isValid } from 'date-fns';
 import { parse } from 'date-fns/fp';
+import { Player } from '../../usecase/types/player';
 import { Reply, ReplyType } from '../../usecase/types/reply';
+import { playerController } from '../queries/player';
 
 const parseDate = parse(new Date(), 'yyyy-MM-dd');
 
-export const commandResult = (args: string[]): Reply => {
+export const commandResult = async (args: string[]): Promise<Reply> => {
   if (args.length < 8) {
     return {
       type: ReplyType.Error,
@@ -12,10 +14,14 @@ export const commandResult = (args: string[]): Reply => {
         'Invalid Arguments: Date, EntryCount, Stack, Players...(4 or more)',
     };
   }
+
   const date = parseDate(args[1]);
   const entryCount = parseInt(args[2]);
   const stack = parseInt(args[3]);
-  const players = args.slice(4);
+  const playerNameList = args.slice(4);
+  const playerList: Player[] = [];
+
+  // validation
   if (!isValid(date) || date.getFullYear() < 2000) {
     return {
       type: ReplyType.Error,
@@ -28,12 +34,28 @@ export const commandResult = (args: string[]): Reply => {
       errorText: 'Error: entry count and stack must be numbers',
     };
   }
-  if (entryCount != players.length) {
+  if (entryCount != playerNameList.length) {
     return {
       type: ReplyType.Error,
       errorText: 'Error: entry count and number of players do not match',
     };
   }
+  for (let i = 0; i < playerNameList.length; i++) {
+    try {
+      const player = await playerController.readByDiscordOrName(
+        playerNameList[i]
+      );
+      playerList.push(player);
+    } catch (error) {
+      return {
+        type: ReplyType.Error,
+        errorText: `${error}`,
+      };
+    }
+  }
+
+  // insert data
+
   return {
     type: ReplyType.Error,
     errorText: 'Error: Not Implemented',

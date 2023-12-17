1fef4b534d1faf4f2f9ba061e7d1bb120bd61f80
claustra01 <shinya430.g@gmail.com>
2023-12-17 21:52:07 +0900

    [wip] insert game and calc data

--- src/adapter/commands/result.ts
+++ src/adapter/commands/result.ts
@@ -1,10 +1,45 @@
 import { isValid } from 'date-fns';
 import { parse } from 'date-fns/fp';
+import { config } from '../../config/config';
+import { Calculate, NewCalculate } from '../../usecase/types/calculate';
+import { Game, NewGame } from '../../usecase/types/game';
 import { Player } from '../../usecase/types/player';
 import { Reply, ReplyType } from '../../usecase/types/reply';
+import { calculateController } from '../queries/calculate';
+import { gameController } from '../queries/game';
 import { playerController } from '../queries/player';
+import { transactionController } from '../queries/transaction';
 
 const parseDate = parse(new Date(), 'yyyy-MM-dd');
+
+const insertGame = async (newData: NewGame): Promise<Game> => {
+  return await gameController
+    .create(newData)
+    .then((game) => {
+      return game;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const insertCalc = async (newData: NewCalculate): Promise<Calculate> => {
+  return await calculateController
+    .create(newData)
+    .then((game) => {
+      return game;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const isExcluded = (gameId: number, playerGameCount: number): boolean => {
+  const calculatingGameCount = config.calculatingGameCount;
+  if (gameId <= 5) return false;
+  if (playerGameCount <= calculatingGameCount) return true;
+  return false;
+};
 
 export const commandResult = async (args: string[]): Promise<Reply> => {
   if (args.length < 8) {
@@ -15,14 +50,20 @@ export const commandResult = async (args: string[]): Promise<Reply> => {
     };
   }
 
-  const date = parseDate(args[1]);
+  const date = args[1];
+  const parsedDate = parseDate(date);
   const entryCount = parseInt(args[2]);
   const stack = parseInt(args[3]);
   const playerNameList = args.slice(4);
   const playerList: Player[] = [];
+  const calcList: Calculate[] = [];
 
   // validation
-  if (!isValid(date) || date.getFullYear() < 2000) {
+  if (
+    !isValid(parsedDate) ||
+    date.length !== 10 ||
+    parsedDate.getFullYear() < 2000
+  ) {
     return {
       type: ReplyType.Error,
       errorText: 'Error: Invalid date format: YYYY-MM-DD',
@@ -55,9 +96,42 @@ export const commandResult = async (args: string[]): Promise<Reply> => {
   }
 
   // insert data
+  try {
+    await transactionController.begin();
+    const game = await insertGame({
+      date,
+      entryCount,
+      stack,
+    });
+    for (let i = 0; i < playerList.length - 1; i++) {
+      for (let j = 1; j < playerList.length; j++) {
+        if (i < j) {
+          const calc = await insertCalc({
+            gameId: game.gameId,
+            winnerName: playerList[i].playerName,
+            winnerRate: playerList[i].currentRate,
+            winnerIsExcluded: isExcluded(game.gameId, playerList[i].gameCount),
+            loserName: playerList[j].playerName,
+            loserRate: playerList[j].currentRate,
+            loserIsExcluded: isExcluded(game.gameId, playerList[j].gameCount),
+          });
+          calcList.push(calc);
+        }
+      }
+    }
+    await transactionController.commit();
+  } catch (error) {
+    await transactionController.rollback();
+    return {
+      type: ReplyType.Error,
+      errorText: `${error}`,
+    };
+  }
+
+  // calculate rate
 
   return {
     type: ReplyType.Error,
-    errorText: 'Error: Not Implemented',
+    contentText: 'Not Implemented',
   };
 };

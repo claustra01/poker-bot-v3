d9b04f960e9da3cc3df71c387a7b02869cd01626
claustra01 <shinya430.g@gmail.com>
2023-12-17 20:41:00 +0900

    [wip] result command validation

--- src/adapter/commands/result.ts
+++ src/adapter/commands/result.ts
@@ -1,7 +1,39 @@
+import { isValid } from 'date-fns';
+import { parse } from 'date-fns/fp';
 import { Reply, ReplyType } from '../../usecase/types/reply';
 
+const parseDate = parse(new Date(), 'yyyy-MM-dd');
+
 export const commandResult = (args: string[]): Reply => {
-  console.log(args);
+  if (args.length < 8) {
+    return {
+      type: ReplyType.Error,
+      errorText:
+        'Invalid Arguments: Date, EntryCount, Stack, Players...(4 or more)',
+    };
+  }
+  const date = parseDate(args[1]);
+  const entryCount = parseInt(args[2]);
+  const stack = parseInt(args[3]);
+  const players = args.slice(4);
+  if (!isValid(date) || date.getFullYear() < 2000) {
+    return {
+      type: ReplyType.Error,
+      errorText: 'Error: Invalid date format: YYYY-MM-DD',
+    };
+  }
+  if (isNaN(entryCount) || isNaN(stack)) {
+    return {
+      type: ReplyType.Error,
+      errorText: 'Error: entry count and stack must be numbers',
+    };
+  }
+  if (entryCount != players.length) {
+    return {
+      type: ReplyType.Error,
+      errorText: 'Error: entry count and number of players do not match',
+    };
+  }
   return {
     type: ReplyType.Error,
     errorText: 'Error: Not Implemented',

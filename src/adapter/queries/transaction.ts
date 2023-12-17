05e2c0900b5689b7a9028ea09777f787f3a72da0
claustra01 <shinya430.g@gmail.com>
2023-12-17 21:50:48 +0900

    [add] transaction controller

---
+++ src/adapter/queries/transaction.ts
@@ -0,0 +1,29 @@
+import { IDatabase } from '../../usecase/interfaces/db';
+
+export class TransactionController {
+  private pool!: IDatabase;
+
+  setPool(pool: IDatabase): void {
+    this.pool = pool;
+  }
+
+  async begin(): Promise<void> {
+    await this.pool.query(`BEGIN TRANSACTION`).catch((error) => {
+      throw error;
+    });
+  }
+
+  async commit(): Promise<void> {
+    await this.pool.query(`COMMIT TRANSACTION`).catch((error) => {
+      throw error;
+    });
+  }
+
+  async rollback(): Promise<void> {
+    await this.pool.query(`ROLLBACK TRANSACTION`).catch((error) => {
+      throw error;
+    });
+  }
+}
+
+export const transactionController = new TransactionController();
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { calculateController } from './adapter/queries/calculate';
 import { gameController } from './adapter/queries/game';
 import { playerController } from './adapter/queries/player';
+import { transactionController } from './adapter/queries/transaction';
 import { config } from './config/config';
 import { runDiscordBot } from './infrastructure/discord';
 import { PgPool } from './infrastructure/postgres';
@@ -12,6 +13,7 @@ config.load();
 
 const pool = new PgPool();
 pool.connect();
+transactionController.setPool(pool);
 playerController.setPool(pool);
 gameController.setPool(pool);
 calculateController.setPool(pool);
